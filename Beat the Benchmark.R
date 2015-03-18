a28652aff8a4fcdd6d2a0798c607c6fa9c9effcf
ujwlkarn <ujwlkarn@gmail.com>
2015-03-18 23:25:11 +0530

    code modified

--- Beat the Benchmark.R
+++ Beat the Benchmark.R
@@ -1,7 +1,9 @@
 library(nnet)
+set.seed(342)
 train<-read.csv("train.csv")
 test<-read.csv("test.csv")
 fit<-nnet(target ~ ., train[,-1], size = 3, rang = 0.1, decay = 5e-4, maxit = 500) 
 predicted<-as.data.frame(predict(fit,test[,-1],type="raw"))  
-output<-cbind(test[,1],predicted) 
+id<-test[,1]
+output<-cbind(id,predicted) 
 write.csv(output,"submission.csv",row.names=FALSE)
