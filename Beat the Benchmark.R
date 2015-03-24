996a6b9b78db78e7645854e603728058a8b1cb36
Ujjwal Karn <ujwlkarn@gmail.com>
2015-03-24 10:24:11 +0530

    Update Beat the Benchmark.R

--- Beat the Benchmark.R
+++ Beat the Benchmark.R
@@ -4,10 +4,19 @@ library(nnet)
 
 #set the seed so that the results are reproducible
 set.seed(342)
+
+#read the data
 train<-read.csv("train.csv")
 test<-read.csv("test.csv")
+
+#fit a neural network model
+#try different values of the parameters to obtain a better score 
 fit<-nnet(target ~ ., train[,-1], size = 3, rang = 0.1, decay = 5e-4, maxit = 500) 
+
+#predict on the test data
 predicted<-as.data.frame(predict(fit,test[,-1],type="raw"))  
+
+#create the submission file
 id<-test[,1]
 output<-cbind(id,predicted) 
 write.csv(output,"submission.csv",row.names=FALSE)
